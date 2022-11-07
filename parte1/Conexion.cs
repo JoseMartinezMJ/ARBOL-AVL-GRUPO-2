bb8f116a19131268af96f89e54ba04f73c317d12
Jose Martinez <102710668+JoseMartinezMJ@users.noreply.github.com>
2022-11-06 23:01:43 -0600

    Add files via upload

---
+++ parte1/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+namespace AVL
+{
+    public class Conexion
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server = localhost; User Id = postgres; Password = Jamj.2003; Database = postgres");
+        public void Conectar()
+        {
+            conn.Open();
+            System.Windows.Forms.MessageBox.Show("Listo");
+        }
+    }
+}
